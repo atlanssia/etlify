ce6245ade6d056f0dd09e6ab671279497707fe4e
atlanssia <hi@atlanssia.com>
2024-09-14 00:00:09 +0800

    sys

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
+use chrono::{TimeZone, Utc};
+use humansize::BINARY;
 use serde_derive::Serialize;
 use std::env;
 use sysinfo::System;
@@ -19,8 +21,29 @@ async fn main() {
     let mut sys = System::new_all();
     sys.refresh_all();
 
-    info!("total memory: {} bytes", sys.total_memory());
-    info!("global_cpu_usage: {}", sys.global_cpu_usage());
+    let formatter = humansize::make_format(BINARY);
+
+    info!("global_cpu_usage: {}%", sys.global_cpu_usage());
+    info!("total memory: {}", formatter(sys.total_memory()));
+    info!("free memory: {}", formatter(sys.free_memory()));
+    info!("used memory: {}", formatter(sys.used_memory()));
+    info!("available memory: {}", formatter(sys.available_memory()));
+    info!("uptime: {}", System::uptime());
+
+    let datetime = Utc
+        .timestamp_opt(System::boot_time() as i64, 0)
+        .unwrap()
+        .to_rfc3339();
+
+    info!("boot time: {}", datetime);
+    info!("name: {}", System::name().unwrap());
+    info!("os version: {}", System::long_os_version().unwrap());
+
+    let load_avg = System::load_average();
+    info!(
+        "one minute: {}%, five minutes: {}%, fifteen minutes: {}%",
+        load_avg.one, load_avg.five, load_avg.fifteen
+    );
 
     // web app
     let app = Router::new()
