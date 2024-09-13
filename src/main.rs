844f1286990cc446f9d907ddf2a4e8beb106b127
atlanssia <huangronghua@szzhyx.net>
2024-09-13 20:10:50 +0800

    web

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,51 @@
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use serde_derive::Serialize;
 use std::env;
+use sysinfo::System;
+use tokio::net::TcpListener;
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt::init();
+    info!("Starting...");
+    info!("OS: {}", env::consts::OS);
+    info!("ARCH: {}", env::consts::ARCH);
 
-    let config_path = env::var("CONFIG_PATH").unwrap_or_else(|_| "config.toml".to_string());
+    let mut sys = System::new_all();
+    sys.refresh_all();
 
-    match 1 {
-            num @ (1 | 2) => {
-                println!("{}", num);
-            }
-            _ => {}
-        }
+    info!("total memory: {} bytes", sys.total_memory());
+    info!("global_cpu_usage: {}", sys.global_cpu_usage());
 
-    // let c = config::load_config();
+    // web app
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/status", get(status));
 
-    // server::server();
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
 
-    // 使用配置创建服务
-    // let nats_service = NatsService::new(&config);
+async fn root() -> (StatusCode, String) {
+    (StatusCode::OK, String::from("ok"))
+}
 
-    // 其他操作...
+async fn status() -> (StatusCode, Json<Status>) {
+    let stat = Status {
+        status: 0,
+        description: String::from("running"),
+    };
 
-    // Ok(())
+    (StatusCode::OK, Json(stat))
+}
+
+#[derive(Serialize)]
+struct Status {
+    status: i8,
+    description: String,
 }
